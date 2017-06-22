995db5df8ff925dfe38f8e08358a9e4f6b9e2b1a
liuz <liuzhuan@qingsongchou.com>
2017-06-22 13:21:09 +0800

    complete TreePicker component

--- test/ustbhuangyi-picker/js/TreePicker.js
+++ test/ustbhuangyi-picker/js/TreePicker.js
@@ -21,24 +21,24 @@ class TreePicker {
             ) {
             this._selectedValue = options.selectedValue.slice(0, options.numCols);
         }
-        
+
+        /** 当未指定初始选中值时，使用的默认索引值 */
+        this._defaultIndex = new Array(this._numCols).fill(0);
+        /** 查找初始值的索引值 */
         this._selectedIndex = this._findSelectedIndex(this._data, this._selectedValue);
-        this._selectedItems = this._findSelectedItems(this._selectedIndex);
+        /** 查找当前选中的元素 */
+        this._selectedItems = this._findSelectedItems(this._data, this._selectedIndex);
 
         this.initPicker();
     }
 
     /** 初始化 Picker */
     initPicker() {
-        const selectedIndex = this._findSelectedIndex(this._data, this._selectedValue);
-        const data = this._getColumnDatas(this._data, selectedIndex);
-        console.log('before picker: ', data);
-
-        if (!data) return;
+        const columnDatas = this._getColumnDatas(this._data, this._selectedIndex);
 
         this.picker = new Picker({
-            data: data,
-            selectedIndex: selectedIndex,
+            data: columnDatas,
+            selectedIndex: this._selectedIndex,
             title: this._title
         });
 
@@ -48,11 +48,14 @@ class TreePicker {
     initEvents(picker) {
         var self = this;
         picker.on('picker.select', function(selectedVal, selectedIndex) {
-
+            self._selectedItems = self._findSelectedItems(self._data, selectedIndex);
+            if (self._onselect) {
+                self._onselect(self._selectedItems);
+            }
         });
 
         picker.on('picker.change', function(index, selectedIndex) {
-
+            self._updateColumns(index, selectedIndex);
         });
     }
 
@@ -62,57 +65,101 @@ class TreePicker {
         }
     }
 
-    _getColumnDatas(datas, indexes, i = 0) {
-        if (!indexes) return;
+    _updateColumns(index, selectedIndex) {
+        console.log('update column data: ', index, selectedIndex);
+        this._selectedIndex[index] = selectedIndex;
 
-        var items = datas.map(item => (
-            { text: item.text, value: item.value }
-        ));
-        
-        var currentIndex = 0;
-        if (indexes && indexes.length > 0) {
-            currentIndex = indexes[i];
+        if (index >= this._numCols - 1) {
+            return;
         }
+
+        var stillIndex = this._selectedIndex.slice(0, index+1);
+        var updatedIndex = this._selectedIndex.slice(index+1);
         
-        var currentItem = datas[currentIndex];
-        
-        if (!currentItem) {
-            return items;
-        }
-        
-        if (!currentItem.hasOwnProperty('sub')) {
-            return items;
-        }
-        
-        if (i + 1 >= indexes.length) {
-            return items;
-        }
-        
-        var subdatas = currentItem.sub;
-        var subitems = getColumnDatas(subdatas, indexes, i+1);
-        return [items].concat([subitems]);
+        var sourcedata = this._data;
+        stillIndex.forEach(function(item){
+            sourcedata = sourcedata[item].sub;
+        });
+
+        var updatedColumns = this._getColumnDatas(sourcedata, updatedIndex);
+        // console.log('updated columns:', updatedColumns);
+
+        var picker = this.picker;
+        updatedColumns.forEach(function(item, i){
+            let column = item;
+            let idx = index + i + 1;
+            console.log(idx, column);
+            picker.refillColumn(idx, column);
+            picker.scrollColumn(idx, 0);
+        });
+    }
+
+    _getColumnDatas(datas, index) {
+        let res = [];
+        let sourcedata = datas;
+        index.forEach(function(item) {
+            var column = sourcedata.map(function(item){
+                return {
+                    text: item.text,
+                    value: item.value
+                }
+            });
+            res.push(column);
+
+            if (!sourcedata[item]) {
+                sourcedata = sourcedata[0].sub
+            } else {
+                sourcedata = sourcedata[item].sub;
+            }
+        });
+        return res;
     }
 
     /** 返回选中的元素数据，每个数组元素格式为：`{ text: "text", value: "23" }` */
-    _findSelectedItems(values) {
-        // TODO: return the selected items
+    _findSelectedItems(datas, index) {
+        var res = [];
+        var lastItem = null;
+        index.forEach(function(item, idx) {
+            var item = lastItem ? lastItem[item] : datas[item];
+            res.push({
+                text: item.text,
+                value: item.value
+            });
+            lastItem = item.sub;
+        });
+
+        return res;
     }
 
-    /** 搜索选中的索引值 */
+    /**
+     * 从 datas 中查找 values 指定的数值所在的索引值
+     * 若 values == null, 则返回 `_defaultIndex`
+     * 若数组对应的值未找到，则后面的所有数值都是 0
+     * @param {Array} datas 待查找数组
+     * @param {Array} values 待匹配数值数组
+     * @param {Number} i 层级深度 
+     */
     _findSelectedIndex(datas, values, i = 0) {
         if (!datas) return;
-        if (!values) return;
-        if (values.length <= 0) return;
+        if (!values) return this._defaultIndex;
+        if (values.length <= 0) return this._defaultIndex;
         
         var currentValue = values[i];
-        if (!currentValue) return;
+        if (currentValue == undefined) {
+            currentValue = 0;
+        }
         
-        var currentIndex = findIndexFromArray(datas, 'value', currentValue);
-        if (currentIndex == -1) return [0];
+        var currentIndex = datas.findIndex(function(item){ 
+            return item.value == currentValue 
+        });
+        if (currentIndex == -1) {
+            let remainingArr = (values.length - i);
+            return new Array(remainingArr).fill(0);
+        }
         
         var res = [currentIndex];
         if (i < values.length - 1 && datas[currentIndex].hasOwnProperty('sub')) {
-            var subres = findSelectedIndex(datas[currentIndex].sub, values, i + 1);
+            var subres = this._findSelectedIndex(datas[currentIndex].sub, values, i + 1);
             return res.concat(subres);
         } else {
             return res;
@@ -134,9 +181,26 @@ class TreePicker {
         res.forEach(function(item) {
             var pid = item.value;
             var citydata = cdata[pid];
-            console.log(citydata);
-        })
+            
+            var cityItems = obj2arr(citydata, 'name');
+            if (cityItems) {
+                item.sub = cityItems;
 
+                cityItems.forEach(function(item) {
+                    var cityid = item.value;
+                    var areadata = citydata[cityid];
+                    if (areadata.area) {
+                        // console.log('areadata:', areadata.area);
+                        var areaitem = obj2arr(areadata.area, 'name');
+                        if (areaitem) {
+                            item.sub = areaitem;
+                        }
+                    }
+                })
+            }
+        });
+
+        /** 将 obj 对象转换为数组  */
         function obj2arr(obj, attr = null) {
             if (!obj) return null;
 
@@ -157,20 +221,4 @@ class TreePicker {
 
         return res;
     }
-}
-
-/**
- * 寻找匹配元素所在索引值，如果没有找到，返回 -1
- * @param {Array} datas 原始数据 
- * @param {String} key 需要匹配的键值
- * @param {*} value 需要匹配的数值
- */
-function findIndexFromArray(datas, key, value) {
-  for (var i = 0; i < datas.length; i++) {
-    if (datas[i][key] == value) {
-      return i;
-    }
-  }
-  
-  return -1;
 }
