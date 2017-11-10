70b9dbeee02d761c8e4d74228bf8559f9a942bcb
liuz <liuzhuan@qingsongchou.com>
2017-11-10 15:47:20 +0800

    updating unload event handler

--- test/onunload/main.js
+++ test/onunload/main.js
@@ -1,6 +1,8 @@
 window.addEventListener('beforeunload', function(e){
     console.log('do something!');
 
+    document.body.style.background = 'steelblue'
+
     const msg = 'Please Don\'t Go!'
     e.returnValue = msg
     return msg
