055d113fe08787aedebc81c3dfcc5e1b77e972b3
liuz <liuzhuankyzy@gmail.com>
2018-02-20 09:51:10 +0800

    hacking lyubishchev time tool

---
+++ tools/lyubishchev-time/index.js
@@ -0,0 +1,11 @@
+const vm = new Vue({
+    el: '#app',
+    data: {
+        log: [
+            {
+                label: '吃饭',
+                time: Date.now()
+            }
+        ]
+    }
+})
