ceca7dcfa0776c8b14a0f24b457ea671468a87e2
liuz <liuzhuan@qingsongchou.com>
2018-05-07 18:57:36 +0800

    adding load image promise

--- test/html2canvas-demo/main.js
+++
@@ -1,4 +0,0 @@
-var imgs = [
-    'http://thumb.qschou.com/files/qschou.com/project/261/c05de4e2-3277-49ef-afb6-ef576db691e7/151445719592963c0337.jpeg@!home.png',
-    'http://thumb.qschou.com/files/qschou.com/project/848/98610874-684e-4b25-bc35-de956ca35a67/15143449701756f7e0d2.png@!home.png'
-]
